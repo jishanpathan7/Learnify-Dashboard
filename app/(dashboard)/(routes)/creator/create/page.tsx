7e2b2e4c70dd75123f13b796e9f5867a2535389b
Jishan Pathan <88240594+jishanpathan7@users.noreply.github.com>
2023-12-04 19:23:07 +0530

    fix: eslint issues

--- app/(dashboard)/(routes)/creator/create/page.tsx
+++ app/(dashboard)/(routes)/creator/create/page.tsx
@@ -51,7 +51,7 @@ const CreatePage = () => {
       <div>
         <h1 className="text-2xl">Name your Course</h1>
         <p className="text-sm text-slate-600">
-          What would you like to name your course? Don't worry, you can change
+          What would you like to name your course? Dont worry, you can change
           this later.{" "}
         </p>
         <Form {...form}>
